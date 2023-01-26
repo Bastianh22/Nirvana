b5939713bf620aa4db3cf1e6d918c0af30cbfbd5
Bastianh22 <81431298+Bastianh22@users.noreply.github.com>
2023-01-26 16:30:43 +0100

    voir plus

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var tabContents = document.getElementsByClassName("tab-contents");
 
 function openTab(tabName) {
   console.log('bonjour')
-  for(tabLink of tabLinks) {
+  for (tabLink of tabLinks) {
     tabLink.classList.remove("active-link")
   }
-  for(tabContent of tabContents) {
+  for (tabContent of tabContents) {
     tabContent.classList.remove("active-tab")
   }
   event.currentTarget.classList.add("active-link")
@@ -21,4 +21,21 @@ function openMenu() {
 
 function closeMenu() {
   sideMenu.style.right = "-170px";
+}
+
+function afficher_cacher() {
+  console.log('bonjour')
+  bouton = document.getElementById('bouton_texte');
+  texte = document.getElementById('texte');
+  if (texte.style.display == "block") {
+    console.log('au revoir2')
+    texte.style.display = "none";
+    bouton.innerHTML = 'Voir plus';
+  }
+  else {
+    console.log('au revoir')
+    texte.style.display = "block";
+    bouton.innerHTML = 'Voir moins';
+  }
+  return true;
 }
